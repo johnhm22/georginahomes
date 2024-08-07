6ebdc7bd5b233da0af36d9f3c3b2525b6fa54a97
john <johnhowardm@hotmail.com>
2024-08-07 16:25:01 +0100

    Adding slice to home page

--- app/page.tsx
+++ app/page.tsx
@@ -21,19 +21,13 @@ import { Metadata } from 'next';
 import { SliceZone } from '@prismicio/react';
 
 import { createClient } from '@/prismicio';
-// import { components } from '@/slices';
+import { components } from '@/slices';
 
 export default async function Page() {
   const client = createClient();
   const page = await client.getSingle('homepage');
 
-  return (
-    <div className='py-24 text-3xl text-blue-500'>
-      Georgina Homes - it works!
-    </div>
-  );
-
-  // return <SliceZone slices={page.data.slices} components={components} />;
+  return <SliceZone slices={page.data.slices} components={components} />;
 }
 
 export async function generateMetadata(): Promise<Metadata> {

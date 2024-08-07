cce0c7c36e83711b15f6d89ee8328f319e5d4ecd
john <johnhowardm@hotmail.com>
2024-08-07 16:24:21 +0100

    Updating to leave copyright only

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,18 +7,20 @@ const Footer = async () => {
   const client = createClient();
   const settings = await client.getSingle('settings');
   return (
-    <footer className='flex flex-row justify-between px-2'>
-      <Link href='/'>{settings.data.site_title}</Link>
+    <footer className='flex flex-row justify-center px-2'>
+      {/* <Link className='font-bold' href='/'>
+        {settings.data.site_title}
+      </Link> */}
       <p className='text-xs'>
         &copy; {new Date().getFullYear()} {settings.data.site_title}
       </p>
-      <ul className='flex flex-row gap-4'>
+      {/* <ul className='flex flex-row gap-4 font-semibold'>
         {settings.data.navigation.map(item => (
           <li key={item.label}>
             <PrismicNextLink field={item.link}>{item.label}</PrismicNextLink>
           </li>
         ))}
-      </ul>
+      </ul> */}
     </footer>
   );
 };

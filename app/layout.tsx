0a9944c4734676e1d416c3ba8e2a4f14e226e798
john <johnhowardm@hotmail.com>
2024-08-07 16:24:38 +0100

    Adding nunito font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,23 @@
-import type { Metadata, ResolvingMetadata } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.css';
+import type { Metadata } from 'next';
+import { Nunito, Nunito_Sans } from 'next/font/google';
 import { createClient } from '@/prismicio';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+import clsx from 'clsx';
+import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] });
+// const inter = Inter({ subsets: ['latin'] });
+
+const nunito = Nunito({
+  subsets: ['latin'],
+  variable: '--font-nunito',
+  display: 'swap'
+});
+const nunitoSans = Nunito_Sans({
+  subsets: ['latin'],
+  variable: '--font-nunito-sans',
+  display: 'swap'
+});
 
 export async function generateMetadata(): Promise<Metadata> {
   const client = createClient();
@@ -28,7 +40,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={inter.className}>
+      <body className={clsx(nunito.variable, nunitoSans.variable)}>
         <Header />
         {children}
         <Footer />

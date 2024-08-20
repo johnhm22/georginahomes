a949b28c9756cf4d459031904aa4908cbb28498f
john <johnhowardm@hotmail.com>
2024-08-20 09:32:39 +0100

    Removing unnecessary links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,20 +7,10 @@ const Footer = async () => {
   const client = createClient();
   const settings = await client.getSingle('settings');
   return (
-    <footer className='flex flex-row justify-center px-2'>
-      {/* <Link className='font-bold' href='/'>
-        {settings.data.site_title}
-      </Link> */}
+    <footer className='flex flex-row justify-center border border-blue-500 px-2'>
       <p className='text-xs'>
         &copy; {new Date().getFullYear()} {settings.data.site_title}
       </p>
-      {/* <ul className='flex flex-row gap-4 font-semibold'>
-        {settings.data.navigation.map(item => (
-          <li key={item.label}>
-            <PrismicNextLink field={item.link}>{item.label}</PrismicNextLink>
-          </li>
-        ))}
-      </ul> */}
     </footer>
   );
 };

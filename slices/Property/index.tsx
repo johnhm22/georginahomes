5dcd5d1f7000c5ede99794e564b69b9a1591768f
john <johnhowardm@hotmail.com>
2024-08-26 09:28:10 +0100

    Adding Carousel to slice and removing redundant code

--- slices/Property/index.tsx
+++ slices/Property/index.tsx
@@ -1,10 +1,7 @@
 'use client';
 
 import { Content } from '@prismicio/client';
-import { createClient } from '@/prismicio';
-import { PrismicNextImage } from '@prismicio/next';
 import { PrismicRichText, SliceComponentProps } from '@prismicio/react';
-import { useState } from 'react';
 import Image from 'next/image';
 
 import {
@@ -24,29 +21,6 @@ export type PropertyProps = SliceComponentProps<Content.PropertySlice>;
  * Component for "Property" Slices.
  */
 const Property = async ({ slice }: PropertyProps): Promise<JSX.Element> => {
-  const [imageIndex, setImageIndex] = useState<number>(0);
-
-  const client = createClient();
-
-  const urlArray: string[] = [];
-
-  const homepage = await client.getSingle('homepage', {});
-
-  // console.log('homepage: ', homepage.data.slices);
-
-  homepage.data.slices.forEach(slice => {
-    if (slice.id === 'property$a2a4e8b8-49c4-453d-93c9-838ac731ca5b') {
-      slice.primary.photos.map(photo => {
-        urlArray.push(photo.property_photo.url);
-      });
-      // console.log(slice.primary.photos);
-      // urlObject[slice.id].[slice.photos]
-    }
-  });
-  // console.log('*** urlArray *** ', urlArray);
-
-  // console.log('*** slice ***:', slice);
-
   /*
   Below is field entry for PrismicNextImage
   *** item *** {
@@ -61,33 +35,7 @@ const Property = async ({ slice }: PropertyProps): Promise<JSX.Element> => {
   }
 */
 
-  // console.log('**** slice.primary.photos[0] ****: ', slice.primary.photos[0]);
-
-  const imageArray: Content.PropertySliceDefaultPrimaryPhotosItem[] = [];
-  const displayImages = () => {
-    slice.primary.photos.map(item => {
-      imageArray.push(item);
-    });
-    return null;
-  };
-
-  const sliceArray: Content.PropertySlice[] = [];
-
-  const displaySlices = (slice: Content.PropertySlice) => {
-    if (slice.id === 'property$a2a4e8b8-49c4-453d-93c9-838ac731ca5b') {
-      // setCurrentImage(preVal => [...preVal, slice]);
-    }
-    // console.log('*** sliceArray internal***: ', sliceArray);
-    return null;
-  };
-
-  const handleOnClick = () => {
-    console.log('onClick clicked');
-    // console.log('slice.id from onClick', id);
-    // const clickedImage = imageArray.filter((idx)=> idx.id === id)
-    setImageIndex(imageIndex => imageIndex + 1);
-    console.log('*** imageIndex ***', imageIndex);
-  };
+  console.log('**** slice.primary.photos ****: ', slice.primary.photos);
 
   return (
     <section
@@ -96,33 +44,23 @@ const Property = async ({ slice }: PropertyProps): Promise<JSX.Element> => {
       className='flex justify-center'
     >
       <div className='my-2 flex w-2/3 flex-row gap-2 rounded-lg border p-4 shadow-lg'>
-        {displayImages()}
-        {displaySlices(slice)}
-        {/* <PrismicNextImage field={slice.primary.photos[0]!.property_photo} /> */}
-        <div className='relative z-0'>
-          {slice.primary.photos.length === 1 ? (
-            <PrismicNextImage field={imageArray[0].property_photo} />
-          ) : (
-            // <PrismicNextImage field={imageArray[2].property_photo} />
-            // <PrismicNextImage field={imageArray[currentImage].property_photo} />
-            <Image
-              // src='https://images.prismic.io/georginahomes/ZrnZCUaF0TcGI2wZ_IMG_20210424_135657.jpg?auto=format%2Ccompress&rect=0%2C922%2C3456%2C2765&w=500&h=400'
-              // src='https://images.prismic.io/georginahomes/ZrnZCUaF0TcGI2wZ_IMG_20210424_135657.jpg?auto=format%2Ccompress&rect=0%2C922%2C3456%2C2765&w=500&h=400'
-              src={urlArray[imageIndex]}
-              alt=''
-              width='500'
-              height='400'
-            />
-          )}
-
-          {imageArray.length > 1 ? (
-            <p
-              className='z-1 absolute -right-0 top-44 cursor-pointer pr-2 text-2xl font-bold'
-              onClick={() => handleOnClick()}
-            >
-              X
-            </p>
-          ) : null}
+        <div>
+          <Carousel className='w-full max-w-xs'>
+            <CarouselContent>
+              {slice.primary.photos.map((item, index) => (
+                <CarouselItem key={index}>
+                  <Image
+                    src={item.property_photo.url || ''} //need to enter default
+                    alt={item.property_photo.alt || ''}
+                    height={400}
+                    width={500}
+                  />
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            {slice.primary.photos.length > 1 ? <CarouselPrevious /> : null}
+            {slice.primary.photos.length > 1 ? <CarouselNext /> : null}
+          </Carousel>
         </div>
         <div className='flex w-1/2 flex-col'>
           <div className='mb-2 text-2xl font-bold'>

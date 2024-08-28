71b2e8701c235f66e0ceae1d6119a3919a03ce58
john <johnhowardm@hotmail.com>
2024-08-28 10:00:16 +0100

    Adding header for properties

--- slices/Hero/index.tsx
+++ slices/Hero/index.tsx
@@ -19,36 +19,42 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       data-slice-variation={slice.variation}
       // className='border border-red-500 px-4 py-10 md:px-6 md:py-14 lg:py-16'
     >
-      <div className='flex justify-center'>
-        <div className='absolute px-2 py-2 text-center'>
-          <PrismicRichText
-            field={slice.primary.heading}
-            components={{
-              heading1: ({ children }) => (
-                <h1 className='font-display font-semiboldbold py-6 text-2xl md:text-6xl'>
-                  {children}
-                </h1>
-              )
-            }}
-          />
-          <PrismicRichText
-            field={slice.primary.body}
-            components={{
-              paragraph: ({ children }) => (
-                <p className='font-body text-lg font-normal leading-10 md:text-3xl'>
-                  {children}
-                </p>
-              )
-            }}
+      <div className='flex flex-col justify-center'>
+        <div className='flex justify-center'>
+          <div className='absolute px-2 py-2 text-center'>
+            <PrismicRichText
+              field={slice.primary.heading}
+              components={{
+                heading1: ({ children }) => (
+                  <h1 className='font-display font-semiboldbold py-6 text-2xl md:text-6xl'>
+                    {children}
+                  </h1>
+                )
+              }}
+            />
+            <PrismicRichText
+              field={slice.primary.body}
+              components={{
+                paragraph: ({ children }) => (
+                  <p className='font-body text-lg font-normal leading-10 md:text-3xl'>
+                    {children}
+                  </p>
+                )
+              }}
+            />
+          </div>
+          <PrismicNextImage
+            field={slice.primary.image}
+            className='100vw'
+            // width={1100}
+            // height={900}
+            alt=''
           />
         </div>
-        <PrismicNextImage
-          field={slice.primary.image}
-          className='100vw'
-          // width={1100}
-          // height={900}
-          alt=''
-        />
+
+        <div className='mt-12 flex justify-center text-4xl font-semibold'>
+          {slice.primary.property_heading}
+        </div>
       </div>
     </section>
   );

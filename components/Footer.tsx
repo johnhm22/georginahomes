e0544b627f496111c35b66bf0c69d5721018c16d
john <johnhowardm@hotmail.com>
2024-08-28 09:58:33 +0100

    Removing blue border

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = async () => {
   const client = createClient();
   const settings = await client.getSingle('settings');
   return (
-    <footer className='flex flex-row justify-center border border-blue-500 px-2'>
+    <footer className='flex flex-row justify-center px-2'>
       <p className='text-xs'>
         &copy; {new Date().getFullYear()} {settings.data.site_title}
       </p>

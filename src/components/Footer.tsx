63cc9cd93f3f176559445891728721c5f8fc532e
John Morgan <johnhmorgan@outlook.com>
2024-10-24 14:29:29 +0100

    Tidying up code

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { createClient } from '@/prismicio';
-import { PrismicNextLink } from '@prismicio/next';
-import Link from 'next/link';
 
 const Footer = async () => {
   const client = createClient();
   const settings = await client.getSingle('settings');
   return (
-    <footer className='flex flex-row justify-center px-2'>
+    <footer className='flex flex-row justify-center'>
       <p className='text-xs'>
         &copy; {new Date().getFullYear()} {settings.data.site_title}
       </p>
